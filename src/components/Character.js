93547ff621291d8d5fdde713dc24f15deb1e5b74
Elizabeth Doyle <myoddjourney@gmail.com>
2023-06-02 20:24:30 -0400

    structure setup for character

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,11 +3,56 @@ import React {useState, useEffect } from 'react'
 
 const Character = (props) => {
 
+    
 return (
     <div>Character
+        <p>birth year</p>
+        <p>created</p>
+        <p>edited</p>
+        <p>eye color</p>
+        <p>films</p>
+        <p>gender</p>
+        <p>hair color</p>
+        <p>height</p>
+        <p>homeworld</p>
+        <p>mass</p>
+        <p>name</p>
+        <p>skin color</p>
+        <p>species</p>
+        <p>starships</p>
+        <p>vehicles</p>
+        {
+
+        }
         </div>
 )
 
 }
 
 export default Character
+
+/* 
+birth_year: "19BBY"
+created: "2014-12-09T13:50:51.644000Z"
+edited: "2014-12-20T21:17:56.891000Z"
+eye_color: "blue"
+films: 
+(4) ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi', 'Revenge of the Sith']
+gender: "male"
+hair_color: "blond"
+height: "172"
+homeworld: "http://swapi.dev/api/planets/1/"
+mass: "77"
+name: "Luke Skywalker"
+skin_color: "fair"
+species: 
+[]
+starships: 
+(2) ['http://swapi.dev/api/starships/12/', 'http://swapi.dev/api/starships/22/']
+url: "http://swapi.dev/api/people/1/"
+vehicles: 
+(2) ['http://swapi.dev/api/vehicles/14/', 'http://swapi.dev/api/vehicles/30/']
+[[Prototype]]
+: 
+
+*/
